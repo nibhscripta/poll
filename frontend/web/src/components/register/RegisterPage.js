6c42bcb8ce6e5d40e212d6a8a93d164a8f7b987a
tux <nibhscripta@tutanota.com>
2022-05-18 21:38:16 -0500

    ✏️ improve registration material ui && add additonal yup validation

--- frontend/web/src/components/register/RegisterPage.js
+++ frontend/web/src/components/register/RegisterPage.js
@@ -1,98 +1,115 @@
 import changeTitle from "../../helpers/dom/changeTitle";
 // import { Link, useNavigate } from "react-router-dom";
 // import { useState } from "react";
-import { Grid } from "@mui/material";
+import { Grid, TextField, Button, Paper, Typography } from "@mui/material";
+// import Icon from "@mui/material/Icon";
 import * as Yup from "yup";
-import { Formik, Form } from "formik";
-import TextInput from "../form-ui/TextInput";
+import { useFormik } from "formik";
 // import "./RegisterPage.css";
 // import handleRegister from "./HandleRegister";
 // import RegistrationError from "./RegistrationError";
 import { Container } from "@mui/system";
-
-const INITIAL_FORM_STATE = {
-  username: "",
-  email: "",
-  password: "",
-};
-
-const FORM_VALIDATION = Yup.object().shape({
-  username: Yup.string().required("Required field."),
-  email: Yup.string().email("Invalid email.").required("Required field."),
-  password: Yup.string().required("Required field."),
-});
 
 const RegisterPage = () => {
   // const navigate = useNavigate();
   // const [isError, setIsError] = useState(false);
   // const [error, setError] = useState("");
   changeTitle("Create an account");
+  const register = (vals) => {
+    console.log(vals);
+  };
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+    onSubmit: (values) => register(values),
+    validationSchema: Yup.object({
+      username: Yup.string()
+        .matches(/\w$/, "Only use letters, numbers, and underscores")
+        .required("Required field."),
+      email: Yup.string().email("Invalid email.").required("Required field."),
+      password: Yup.string()
+        .min(12, "Password must at least 12 chatacters.")
+        .required("Required field."),
+    }),
+  });
   return (
-    <Grid container>
+    <Grid container style={{ margin: "40px 0" }}>
       <Grid item xs={12}>
-        <Container maxWidth="xs">
-          <Formik
-            initialValues={{
-              ...INITIAL_FORM_STATE,
-            }}
-            validationSchema={FORM_VALIDATION}
-            onSubmit={(values) => {
-              console.log(values);
-            }}
-          >
-            <Form>
-              <Grid container spacing={2}>
+        <form onSubmit={formik.handleSubmit}>
+          <Container maxWidth="xs">
+            <Paper variant="outlined" style={{ padding: "40px" }}>
+              <Grid container spacing={1}>
                 <Grid item xs={12}>
-                  <TextInput name="username" label="Username" />
+                  <Typography variant="h5" component="div">
+                    Create an account
+                  </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    fullWidth={true}
+                    variant="outlined"
+                    name="username"
+                    label="Username"
+                    autoCapitalize="off"
+                    autoComplete="off"
+                    helperText={
+                      formik.errors.username ? formik.errors.username : " "
+                    }
+                    error={
+                      formik.touched.username && Boolean(formik.errors.username)
+                    }
+                    onChange={formik.handleChange}
+                    defaultValue={formik.values.username}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    fullWidth={true}
+                    variant="outlined"
+                    name="email"
+                    label="Email"
+                    type="text"
+                    autoCapitalize="off"
+                    autoComplete="off"
+                    helperText={formik.errors.email ? formik.errors.email : " "}
+                    error={formik.touched.email && Boolean(formik.errors.email)}
+                    onChange={formik.handleChange}
+                    defaultValue={formik.values.email}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    fullWidth={true}
+                    variant="outlined"
+                    name="password"
+                    label="Password"
+                    type="password"
+                    autoCapitalize="off"
+                    autoComplete="off"
+                    helperText={
+                      formik.errors.password ? formik.errors.password : " "
+                    }
+                    error={
+                      formik.touched.password && Boolean(formik.errors.password)
+                    }
+                    onChange={formik.handleChange}
+                    defaultValue={formik.values.password}
+                  />
+                </Grid>
+                <Grid item xs={8} md={6}>
+                  <Button variant="contained" color="primary" type="submit">
+                    Create
+                  </Button>
                 </Grid>
               </Grid>
-            </Form>
-          </Formik>
-        </Container>
+            </Paper>
+          </Container>
+        </form>
       </Grid>
     </Grid>
-
-    // <div className="register-page">
-    //   <form
-    //     className="register-form"
-    //     // onSubmit={(e) => handleRegister(e, setIsError, setError, navigate)}
-    //   >
-    //     <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-    //     <input
-    //       type="text"
-    //       name="username"
-    //       placeholder="Username"
-    //       autoComplete="off"
-    //       autoCapitalize="off"
-    //     />
-    //     <input
-    //       type="text"
-    //       name="email"
-    //       placeholder="Email"
-    //       autoComplete="off"
-    //       autoCapitalize="off"
-    //     />
-    //     <input
-    //       type="password"
-    //       name="password"
-    //       placeholder="Password"
-    //       autoComplete="off"
-    //       autoCapitalize="off"
-    //     />
-    //     <input
-    //       type="password"
-    //       name="confirm_password"
-    //       placeholder="Confirm password"
-    //       autoComplete="off"
-    //       autoCapitalize="off"
-    //     />
-    //     <button type="submit">Create account</button>
-    //     <div className="register-login-redirect">
-    //       Have an account already? <Link to="/login">Login</Link>
-    //     </div>
-    //   </form>
-    //   {isError && <RegistrationError error={error} />}
-    // </div>
   );
 };
 
