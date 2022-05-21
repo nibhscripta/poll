94006c3e294c53c58c7a4138a103b00a412786d3
tux <nibhscripta@tutanota.com>
2022-05-20 21:49:28 -0500

    ✨ add ability to create and view a users polls in api

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -7,6 +7,7 @@ from alembic import context
 
 from api.database import SQLALCHEMY_DATABASE_URL
 from api.users import models as UserModels
+from api.polls import models as PollModels
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -22,7 +23,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = [UserModels.Base.metadata]
+target_metadata = [UserModels.Base.metadata, PollModels.Base.metadata]
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
