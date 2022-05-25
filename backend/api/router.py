6ef882f4393e85a11087c094ec436a5edeec0a44
tux <nibhscripta@tutanota.com>
2022-05-24 21:18:29 -0500

    ✏️ add polls router

--- backend/api/router.py
+++ backend/api/router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from .users.router import router as UserRouter
 from .authentication.router import router as AuthenticationRouter
+from .polls.router import router as PollsRouter
 
 router = APIRouter(
     prefix='/api'
@@ -10,4 +11,5 @@ router = APIRouter(
 
 router.include_router(UserRouter)
 router.include_router(AuthenticationRouter)
+router.include_router(PollsRouter)
 
